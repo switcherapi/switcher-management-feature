a657acac7ff5eb926985d92db05e979eea500756
petruki <31597636+petruki@users.noreply.github.com>
2023-07-15 14:23:54 -0700

    Refactored env settings and test deps

--- src/environment.ts
+++
@@ -1,9 +0,0 @@
-import { load } from './deps.ts';
-
-await load({ export: true });
-
-const env = Deno.env;
-
-export const APP_PORT = env.get('APP_PORT') || 4000;
-export const SSL_CERT = env.get('SSL_CERT');
-export const SSL_KEY = env.get('SSL_KEY');
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,11 +1,35 @@
 import app from '../src/app.ts';
-import { assertEquals, describe, it, superoak } from './deps.ts';
+import { assertEquals, superoak } from './deps.ts';
 
-describe('API Router', function () {
-  it('should return success', async function () {
+Deno.test({
+  name: 'API Suite - it should return ok',
+  async fn() {
     const request = await superoak(app);
     const response = await request.get('/api/check').expect(200);
 
-    assertEquals(response.body, { message: 'Success' });
-  });
+    assertEquals(response.body, {
+      status: 'ok',
+      sslEnabled: false,
+    });
+  },
+});
+
+Deno.test({
+  name: 'API Suite - it should return ok - ssl enabled',
+  async fn(t) {
+    await t.step('given - env vars', async () => {
+      Deno.env.set('SSL_CERT', 'cert');
+      Deno.env.set('SSL_KEY', 'key');
+    });
+
+    await t.step('test', async () => {
+      const request = await superoak(app);
+      const response = await request.get('/api/check').expect(200);
+
+      assertEquals(response.body, {
+        status: 'ok',
+        sslEnabled: true,
+      });
+    });
+  },
 });
