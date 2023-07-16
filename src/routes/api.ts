eec2d17343447ebecfae179c019f006ff512dc48
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-07-15 17:24:28 -0700

    Updated SSL settings - added Postman resources (#3)

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -6,6 +6,7 @@ router.get('/api/check', ({ response }: Context) => {
   response.status = 200;
   response.body = {
     status: 'ok',
+    releaseTime: Deno.env.get('RELEASE_TIME') || 'today',
     sslEnabled: (Deno.env.get('SSL_CERT') && Deno.env.get('SSL_KEY')) ? true : false,
   };
 });
