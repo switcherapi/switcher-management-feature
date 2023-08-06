f0658aa74c27b9d296a6307c0e3a4d8715bdfdd6
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-08-06 14:32:59 -0700

    Bump Switcher4Deno module (#11)

--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
 export { testing } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
 export type { Middleware } from 'https://deno.land/x/oak@v12.6.0/mod.ts';
-export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.196.0/testing/asserts.ts';
+export { assert, assertEquals, assertFalse, assertObjectMatch } from 'https://deno.land/std@0.197.0/testing/asserts.ts';
 export { superoak } from 'https://deno.land/x/superoak@4.7.0/mod.ts';
-export { Switcher } from 'https://deno.land/x/switcher4deno@v1.0.4/mod.ts';
+export { Switcher } from 'https://deno.land/x/switcher4deno@v1.0.5/mod.ts';
