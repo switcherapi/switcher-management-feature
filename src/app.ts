8ea6f44121f542238cbdde0c78f0d9b1ef36a3ab
petruki <31597636+petruki@users.noreply.github.com>
2023-10-14 13:03:20 -0700

    Added helmet

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Application, load } from './deps.ts';
 import { responseTime, responseTimeLog } from './middleware/index.ts';
+import Helmet from './middleware/helmet.ts';
 import { getEnv } from './utils.ts';
 import routerApi from './routes/api.ts';
 import routerFeature from './routes/feature.ts';
@@ -8,9 +9,11 @@ import FeatureService from './services/feature.ts';
 await load({ export: true, envPath: getEnv('ENV_PATH', '.env') });
 
 const app = new Application();
+const helmet = new Helmet();
 const service = new FeatureService();
 await service.initialize(getEnv('SWITCHER_SNAPSHOT_LOAD', 'true') === 'true');
 
+app.use(helmet.middleware());
 app.use(responseTimeLog);
 app.use(responseTime);
 app.use(routerApi.routes());
