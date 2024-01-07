3f353d64c7aa0313a4a728785028577d37a267c2
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-01-06 20:21:13 -0800

    Bump Deno runtime to 1.39.2 (#28)

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 export { Application, Router } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export type { Middleware, Next } from 'https://deno.land/x/oak@v12.6.1/mod.ts';
 export { Context } from 'https://deno.land/x/oak@v12.6.1/context.ts';
-export { load } from 'https://deno.land/std@0.209.0/dotenv/mod.ts';
+export { load } from 'https://deno.land/std@0.211.0/dotenv/mod.ts';
 export { checkValue, Switcher } from 'https://deno.land/x/switcher4deno@v1.0.6/mod.ts';
-export { bold, cyan, green } from 'https://deno.land/std@0.209.0/fmt/colors.ts';
+export { bold, cyan, green } from 'https://deno.land/std@0.211.0/fmt/colors.ts';
