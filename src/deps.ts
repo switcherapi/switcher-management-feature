511ff82d3192e924ff64b050c2a1a3e80464e65d
petruki <31597636+petruki@users.noreply.github.com>
2024-03-09 12:01:46 -0800

    Fixed wrong dep path - was using local ^^

--- src/deps.ts
+++ src/deps.ts
@@ -4,4 +4,4 @@ export { Context } from 'https://deno.land/x/oak@14.2.0/context.ts';
 export { load } from 'https://deno.land/std@0.218.2/dotenv/mod.ts';
 export { checkValue, Switcher } from 'https://deno.land/x/switcher4deno@v1.0.7/mod.ts';
 export { bold, cyan, green } from 'https://deno.land/std@0.218.2/fmt/colors.ts';
-export { ValidatorFn, ValidatorMiddleware } from '../../../deno/validator4oak/mod.ts';
+export { ValidatorFn, ValidatorMiddleware } from 'https://deno.land/x/validator4oak@v1.0.0/mod.ts';
