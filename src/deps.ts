6bba9bea4d2b813f3e4c0508e40b7ea376afb70a
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-09-14 22:32:06 -0700

    Bump Deno runtime 1.46.3, oak@17.0.0 (#38)

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export { Application, Context, type Middleware, type Next, Router } from 'jsr:@oak/oak@16.1.0';
+export { Application, Context, type Middleware, type Next, Router } from 'jsr:@oak/oak@17.0.0';
 export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.0.2';
 export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.1.0';
-export { load } from 'https://deno.land/std@0.224.0/dotenv/mod.ts';
-export { bold, cyan, green } from 'https://deno.land/std@0.224.0/fmt/colors.ts';
+export { load } from 'jsr:@std/dotenv@0.225.2';
+export { bold, cyan, green } from 'jsr:@std/fmt@1.0.2/colors';
