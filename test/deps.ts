3b339f363ac8f925d16caa23d55ac35e45ce29f3
petruki <31597636+petruki@users.noreply.github.com>
2024-10-09 19:53:22 -0700

    Fixes deno lint on test file

--- test/deps.ts
+++ test/deps.ts
@@ -4,4 +4,5 @@ export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
 export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.5';
 
 // Fixes superdeno@4.9.0 issue
+// deno-lint-ignore no-explicit-any
 (globalThis as any).window = globalThis;
