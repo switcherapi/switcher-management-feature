cb3954a48c25d8652ecb4b4531eb29f07a1ae01c
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-10-09 19:54:50 -0700

    Bump Deno runtime to v2.0.0 (#39)
    * Bump Deno runtime to v2.0.0
    
    * Fixes deno lint on test file

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { Application, Context, type Middleware, type Next, Router } from 'jsr:@oak/oak@17.0.0';
+export { Application, Context, type Middleware, type Next, Router } from 'jsr:@oak/oak@17.1.0';
 export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.0.2';
 export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.1.0';
 export { load } from 'jsr:@std/dotenv@0.225.2';
--- test/deps.ts
+++ test/deps.ts
@@ -1,4 +1,8 @@
-export { type Middleware, testing } from 'jsr:@oak/oak@17.0.0';
+export { type Middleware, testing } from 'jsr:@oak/oak@17.1.0';
 export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.0.2';
 export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
 export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.5';
+
+// Fixes superdeno@4.9.0 issue
+// deno-lint-ignore no-explicit-any
+(globalThis as any).window = globalThis;
