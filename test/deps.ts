c894772c3bd9507f6763742caaac19ddcae5e66e
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-12-15 13:26:08 -0800

    Bump Deno runtime to 2.1.4, oak@17.1.3, switcher-client-deno@2.1.0 (#40)

--- test/deps.ts
+++ test/deps.ts
@@ -1,7 +1,7 @@
-export { type Middleware, testing } from 'jsr:@oak/oak@17.1.0';
-export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.0.2';
+export { type Middleware, testing } from 'jsr:@oak/oak@17.1.3';
+export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.1.0';
 export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
-export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.5';
+export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.9';
 
 // Fixes superdeno@4.9.0 issue
 // deno-lint-ignore no-explicit-any
