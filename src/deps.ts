cf58cad3dd34ad6c55bbe8d726fac01f3c2d4761
petruki <31597636+petruki@users.noreply.github.com>
2025-04-26 13:33:23 -0700

    Bump Deno runtime 2.2.12, added devcontainer settings

--- src/deps.ts
+++ src/deps.ts
@@ -2,4 +2,4 @@ export { Application, Context, type Middleware, type Next, Router } from 'jsr:@o
 export { Client } from 'jsr:@switcherapi/switcher-client-deno@2.1.0';
 export { ValidatorFn, ValidatorMiddleware } from 'jsr:@trackerforce/validator4oak@1.2.0';
 export { load } from 'jsr:@std/dotenv@0.225.3';
-export { bold, cyan, green } from 'jsr:@std/fmt@1.0.6/colors';
+export { bold, cyan, green } from 'jsr:@std/fmt@1.0.7/colors';
