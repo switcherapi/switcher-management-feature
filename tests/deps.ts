db53eccb7a0334e3a70c8cc7393168e223c89cca
petruki <31597636+petruki@users.noreply.github.com>
2025-05-18 16:13:18 -0700

    Bump test dep superoak@5.0.0, removed workaround

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,8 +1,4 @@
 export { type Middleware, testing } from 'jsr:@oak/oak@17.1.4';
 export { Client, StrategiesType } from 'jsr:@switcherapi/switcher-client-deno@2.1.0';
-export { superoak } from 'https://deno.land/x/superoak@4.8.1/mod.ts';
+export { superoak } from 'https://deno.land/x/superoak@5.0.0/mod.ts';
 export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.13';
-
-// Fixes superdeno@4.9.0 issue
-// deno-lint-ignore no-explicit-any
-(globalThis as any).window = globalThis;
