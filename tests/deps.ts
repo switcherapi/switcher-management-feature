ca5ae3ff19947ce96b884a34a5dc292703624aea
petruki <31597636+petruki@users.noreply.github.com>
2025-10-07 19:37:20 -0700

    Bump switcher-client-deno@2.4.0

--- tests/deps.ts
+++ tests/deps.ts
@@ -1,4 +1,4 @@
 export { type Middleware, testing } from 'jsr:@oak/oak@17.1.6';
-export { Client, StrategiesType } from 'jsr:@switcherapi/switcher-client-deno@2.3.1';
+export { Client, StrategiesType } from 'jsr:@switcherapi/switcher-client-deno@2.4.0';
 export { superoak } from 'https://deno.land/x/superoak@5.0.0/mod.ts';
 export { assert, assertEquals, assertFalse, assertObjectMatch } from 'jsr:@std/assert@1.0.14';
